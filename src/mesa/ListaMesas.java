81b2b5c896c01cf6665c3a7fe344d3e9ce1f7f3e
pedroryansc <pedroryangamer@gmail.com>
2024-04-26 02:28:38 -0300

    Continuação da gestão de Mesas

--- src/mesa/ListaMesas.java
+++ src/mesa/ListaMesas.java
@@ -8,8 +8,21 @@ public class ListaMesas {
 		inicio = null;
 	}
 	
-	private Mesa getInicio() {
+	public Mesa getInicio() {
 		return inicio;
+	}
+	
+	public void cadastrar(int capacidade) {
+		Mesa novaMesa = new Mesa(1, capacidade);
+		if(inicio == null)
+			inicio = novaMesa;
+		else {
+			Mesa aux = inicio;
+			while(aux.getProx() != null)
+				aux = aux.getProx();
+			novaMesa.setId(aux.getId() + 1);
+			aux.setProx(novaMesa);
+		}
 	}
 	
 }
--- src/mesa/Mesa.java
+++ src/mesa/Mesa.java
@@ -1,31 +1,75 @@
 package mesa;
 
-import cliente.Cliente;
-
 public class Mesa {
 
-	private Cliente inicio;
+	private int id;
 	private int capacidade;
 	private int cadeirasOcupadas = 0;
+	private boolean atendida = true;
+	private Mesa prox;
 	
-	public Mesa(int capacidade) {
-		this.capacidade = capacidade;
+	public Mesa(int id, int capacidade) {
+		setId(id);
+		setCapacidade(capacidade);
+	}
+	
+	public boolean estaOcupada() {
+		return cadeirasOcupadas > 0;
 	}
 
-	public Cliente getInicio() {
-		return inicio;
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public int getCapacidade() {
 		return capacidade;
 	}
 
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+
 	public int getCadeirasOcupadas() {
 		return cadeirasOcupadas;
 	}
+
+	public void setCadeirasOcupadas(int cadeirasOcupadas) {
+		this.cadeirasOcupadas = cadeirasOcupadas;
+	}
+
+	public boolean foiAtendida() {
+		return atendida;
+	}
+
+	public void setAtendida(boolean atendida) {
+		this.atendida = atendida;
+	}
 	
-	public boolean temEspaco() {
-		return capacidade > cadeirasOcupadas;
+	public Mesa getProx() {
+		return prox;
+	}
+	
+	public void setProx(Mesa prox) {
+		this.prox = prox;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Mesa [id=");
+		builder.append(id);
+		builder.append(", capacidade=");
+		builder.append(capacidade);
+		builder.append(", cadeirasOcupadas=");
+		builder.append(cadeirasOcupadas);
+		builder.append(", atendida=");
+		builder.append(atendida);
+		builder.append("]");
+		return builder.toString();
 	}
 	
 }
